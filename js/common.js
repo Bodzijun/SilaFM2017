455a8e5b8f9789379b4bc5bdb735ecd350b201d1
Bodzijun <bodzik@gmail.com>
2016-05-05 19:01:44 +0300

    masony

--- js/common.js
+++ js/common.js
@@ -116,3 +116,13 @@ $(document).ready( function() {
 
     });
 });
+
+
+//масонри плитка
+$('.grid').masonry({
+  // set itemSelector so .grid-sizer is not used in layout
+  itemSelector: '.grid-item',
+  // use element for option
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+})
