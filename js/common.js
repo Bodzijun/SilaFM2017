7c38888a41e84e5be5cb81fb3dc00caa083741bc
Bodzijun <bodzik@gmail.com>
2017-01-26 11:14:58 +0200

    просто апдейт

--- js/common.js
+++ js/common.js
@@ -118,17 +118,3 @@ $(document).ready( function() {
 });
 
 
-
-/*  Search */
-
-$(document).ready(function(e) {
-	$("input[placeholder]").placeholder();
-	$("#search-block-form .form-item input").focus(function(){
-		$("#search-block-form").addClass("focus-active");
-			})
-	.blur(function(){
-		$("#search-block-form").removeClass("focus-active");
-	});
-});
-
-/* Конец */
