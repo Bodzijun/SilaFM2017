a4cd37686b45fdb2b0506ede52fe122f45d75032
Bodzijun <bodzik@gmail.com>
2018-04-13 12:34:12 +0300

    last

--- js/common.js
+++ js/common.js
@@ -1,35 +1,7 @@
-$(document).ready(function() {
-
-	//Таймер обратного отсчета
-	//Документация: http://keith-wood.name/countdown.html
-	//<div class="countdown" date-time="2015-01-07"></div>
-	var austDay = new Date($(".countdown").attr("date-time"));
-	$(".countdown").countdown({until: austDay, format: 'yowdHMS'});
-
-    //Всплывающее окно для откправки по аяксу данных
-    $(".fancybox").fancybox();
-
-	//Навигация по Landing Page
-	//$(".top_mnu") - это верхняя панель со ссылками.
-	//Ссылки вида <a href="#contacts">Контакты</a>
-    //$(".top_mnu").navigation();
-
-	//Добавляет классы дочерним блокам .block для анимации
-	//Документация: http://imakewebthings.com/jquery-waypoints/
-	$(".block").waypoint(function(direction) {
-		if (direction === "down") {
-			$(".class").addClass("active");
-		} else if (direction === "up") {
-			$(".class").removeClass("deactive");
-		};
-	}, {offset: 100});
-
-
-
 	//Кнопка "Наверх"
 	//Документация:
 jQuery( document ).ready(function() {
-
+'use strict';
 
 	jQuery(window).scroll(function(){
 		if ( jQuery(document).scrollTop() > 0 ) {
@@ -43,42 +15,10 @@ jQuery( document ).ready(function() {
   });
 });
 
-	//Аякс отправка форм ОБРАТНЫЙ ЗВОНОК
-	//Документация: http://api.jquery.com/jquery.ajax/
-	$("#callback").submit(function() {
-		$.ajax({
-			type: "GET",
-			url: "mail.php",
-			data: $("#callback").serialize()
-		}).done(function() {
-			alert("Спасибо за заявку!");
-			setTimeout(function() {
-				$.fancybox.close();
-			}, 1000);
-		});
-		return false;
-	});
-	//Аякс отправка форм АНКЕТА ДИКТОРА
-	//Документация: http://api.jquery.com/jquery.ajax/
-	$("#anketa").submit(function() {
-		$.ajax({
-			type: "GET",
-			url: "mail_anketa.php",
-			data: $("#anketa").serialize()
-		}).done(function() {
-			alert("Спасибо за анкету!");
-			setTimeout(function() {
-				$.fancybox.close();
-			}, 1000);
-		});
-		return false;
-	});
-
-});
-
 
 //плавное листание до  шапки дикторов
 $(document).ready(function(){
+	'use strict';
 	$("#choose").on("click","a", function (event) {
 		//отменяем стандартную обработку нажатия по ссылке
 		event.preventDefault();
@@ -96,6 +36,7 @@ $(document).ready(function(){
 
 // прикрепить файл
 $(document).on('change', '.btn-file :file', function() {
+	'use strict';
   var input = $(this),
       numFiles = input.get(0).files ? input.get(0).files.length : 1,
       label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
@@ -103,18 +44,27 @@ $(document).on('change', '.btn-file :file', function() {
 });
 
 $(document).ready( function() {
+	'use strict';
     $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
-
         var input = $(this).parents('.input-group').find(':text'),
             log = numFiles > 1 ? numFiles + ' files selected' : label;
 
         if( input.length ) {
             input.val(log);
         } else {
-            if( log ) alert(log);
+            if(log) alert(log);
         }
 
     });
 });
 
 
+
+// переход к табам с другой страницы
+(function($) {
+	'use strict';
+$(function() {
+    var hash = window.location.hash;
+    $('.nav-tabs a[href="' + hash + '"]').tab('show');
+	});
+})(jQuery);
