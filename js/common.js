8a2172933d6c2449be5565adfa1e3f7ebaa33c94
Bogdan <bogdans@iMac-Pro-Bogdan.local>
2019-03-18 14:51:59 +0200

    archive june 2018

--- js/common.js
+++ js/common.js
@@ -18,26 +18,6 @@
 	});
 
 
-	//плавное листание до  шапки дикторов
-	$(document).ready(function () {
-	    'use strict';
-	    $("#choose").on("click", "a", function (event) {
-	        //отменяем стандартную обработку нажатия по ссылке
-	        event.preventDefault();
-
-	        //забираем идентификатор бока с атрибута href
-	        var id = $(this).attr('href'),
-
-	            //узнаем высоту от начала страницы до блока на который ссылается якорь
-	            top = $(id).offset().top;
-
-	        //анимируем переход на расстояние - top за 1500 мс
-	        $('body,html').animate({
-	            scrollTop: top
-	        }, 1500);
-	    });
-	});
-
 	// прикрепить файл
 	$(document).on('change', '.btn-file :file', function () {
 	    'use strict';
